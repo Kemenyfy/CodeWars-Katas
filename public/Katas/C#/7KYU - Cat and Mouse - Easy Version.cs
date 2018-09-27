83d36aa8aba5da04dca07318e425d75225da2ecd
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-09-27 10:00:35 -0400

    Uploaded 7KYU Kata

---
+++ public/Katas/C#/7KYU - Cat and Mouse - Easy Version.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Kata
+{
+  public static string CatMouse(string x)
+  {
+  var answer = x.IndexOf('m') - x.IndexOf('C') > 4 ? "Escaped!" : "Caught!";
+  return answer;
+  }
+}
