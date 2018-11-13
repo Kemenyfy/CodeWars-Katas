0186cdca6c0e0b563d4b2ca28b440e8d5f46abfc
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-11-13 07:56:04 -0500

    Added 7KYU

--- public/Katas/JavaScript/7KYU/Vowel Count.js
+++ public/Katas/JavaScript/7KYU/Vowel Count.js
@@ -3,3 +3,15 @@ function getCount(str) {
     return str.length - str.replace(/[aeiou]/g, "").length;
 
 }
+
+function getCount(str) {
+
+    return str.replace(/[^aeiou]/gi, '').length;
+
+}
+
+function getCount(str) {
+
+    return (str.match(/[aeiou]/ig) || []).length;
+
+}
