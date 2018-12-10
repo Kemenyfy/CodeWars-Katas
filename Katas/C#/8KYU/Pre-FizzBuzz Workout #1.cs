477b45afcd6fc36895e035e08c75be5ff836f794
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-12-09 22:31:32 -0500

    Added 8KYU

---
+++ Katas/C#/8KYU/Pre-FizzBuzz Workout #1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class Kata
+{
+  public static int[] PreFizz(int n) 
+  {
+    var myList = new List<int>();
+    for(var count = 1; count <= n; count++)
+    {
+        myList.Add(count);
+    }
+
+    var array = myList.ToArray();
+
+    return array;
+  }
+}
