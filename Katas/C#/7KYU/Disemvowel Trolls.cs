93cdbb2819550812d5ba7e204931cad1cb2c7842
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-12-22 23:42:44 -0500

    Added 7KYU

--- Katas/C#/7KYU/Disemvowel Trolls.cs
+++ Katas/C#/7KYU/Disemvowel Trolls.cs
@@ -1,26 +1,11 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Kata
 {
   public static string Disemvowel(string str)
   {
-      var temp = str;
-      var vowels = new string[] 
-      {
-         "a","A","e","E","u","U","i","I","o","O",
-      };
-
-      foreach(var letter in vowels)
-      {
-          if (str.Contains(letter))
-          {
-              temp = str.Replace(letter, string.Empty);
-          }
-
-          str = temp;
-       } 
-
-       return str;
+    return Regex.Replace(str,"[aeiouAEIOU]", "");
   }
   
 }
