a5db4505e0e0d5f690e84089143005aa17d1f436
Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-01-17 21:00:42 -0500

    Added 7KYU

---
+++ Katas/JavaScript/7KYU/Find the divisors!.js
@@ -0,0 +1,13 @@
+function divisors(integer) {
+
+    let answer = [];
+
+    for (let i = 2; i < integer; i++) {
+
+        if (integer % i == 0) answer.push(i);
+
+    }
+
+    return answer.length == 0 ? integer + " is prime" : answer;
+
+};
