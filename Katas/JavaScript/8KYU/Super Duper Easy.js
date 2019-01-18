fc4274244ba9bf2ecf848eca9a2b48d6df03984c
Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-01-17 20:53:23 -0500

    Added 8KYU

--- Katas/JavaScript/8KYU/Super Duper Easy.js
+++ Katas/JavaScript/8KYU/Super Duper Easy.js
@@ -3,3 +3,15 @@ function problem(x) {
     return typeof x === "number" ? x * 50 + 6 : "Error";
 
 }
+
+
+function problem(x) {
+
+    if (typeof x === 'string') {
+        return "Error";
+    }
+    else {
+        return (x * 50) + 6;
+    }
+
+}
