064b5fe4c5ea5c286f464d96fbb2eae3eb8ccc1b
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-08-01 13:38:27 -0400

    Added 8KYU

---
+++ Katas/C#/8KYU/Century From Year.cs
@@ -0,0 +1,21 @@
+public static class Kata
+{
+    public static int CenturyFromYear(int year)
+    {
+        return (year / 100) + ((year % 100 == 0) ? 0 : 1);
+    }
+}
+
+
+public static class Kata
+{
+    public static int CenturyFromYear(int year) => (year - 1) / 100 + 1;
+}
+
+public static class Kata
+{
+    public static int CenturyFromYear(int year)
+    {
+        return (year - 1) / 100 + 1;
+    }
+}
