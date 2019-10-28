ca8ba02805ba84bf120291697d70c500f3839f6e
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-10-28 11:22:53 -0400

    Added 7KYU

--- Katas/JavaScript/7KYU/Remove Duplicate Words.js
+++ Katas/JavaScript/7KYU/Remove Duplicate Words.js
@@ -1,8 +1,18 @@
 function removeDuplicateWords(s) {
+
     let newArray = s.split(' ')
     newArray = newArray.filter(function (element, i) {
         return newArray.indexOf(element) == i
     })
     return newArray.join(' ')
+
+}
+
+// Another Solution:
+
+
+function removeDuplicateWords(s) {
+
+    return [...new Set(s.split(' '))].join(' ');
 
 }
