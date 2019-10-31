33be0383e868132c4e2aeeabc3d79768011ddf1b
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-10-31 14:01:44 -0400

    Added 8KYU

---
+++ Katas/JavaScript/8KYU/A Strange Trip to the Market.js
@@ -0,0 +1,9 @@
+function isLockNessMonster(s) {
+
+    return s.includes("tree fiddy") || s.includes("3.50");
+
+}
+
+// In new format
+
+const isLockNessMonster = (s) => s.includes("tree fiddy") || s.includes("3.50");
