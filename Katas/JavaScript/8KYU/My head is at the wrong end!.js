04182337810bde516fded6747743e84b09f50cf1
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-07 10:13:14 -0500

    Added 8KYU

--- Katas/JavaScript/8KYU/My head is at the wrong end!.js
+++ Katas/JavaScript/8KYU/My head is at the wrong end!.js
@@ -3,3 +3,4 @@ const fixTheMeerkat = (arr) => arr.reverse();
 // Another Solution
 
 //const fixTheMeerkat = ([tail, body, head]) => [head, body, tail];
+//const fixTheMeerkat = (arr) => [arr[2], arr[1], arr[0]];
