ffe60f06a23a0d6029de42d5dc84e980b257b984
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-08 08:41:31 -0500

    Added 8KYU

---
+++ Katas/C#/8KYU/Convert a String to a Number!.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Kata
+{
+    public static int StringToNumber(String str)
+    {
+        return Convert.ToInt32(str);
+    }
+}
+
+//Other Solutions:
+
+//using System;
+
+//public class Kata
+//{
+//    public static Int32 StringToNumber(String Input) => Int32.Parse(Input);
+//}
