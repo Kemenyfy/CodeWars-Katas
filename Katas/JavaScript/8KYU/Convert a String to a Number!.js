e36995c547f6b723d74b154a39f8e5a5c60a68cc
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-08 08:37:25 -0500

    Added 8KYU

---
+++ Katas/JavaScript/8KYU/Convert a String to a Number!.js
@@ -0,0 +1,12 @@
+const stringToNumber = (str) => Number(str);
+
+// Other Solutions:
+
+//var stringToNumber = function (str) {
+//    return parseInt(str);
+//}
+
+//var stringToNumber = function (str) {
+//    return +str;
+//}
+
