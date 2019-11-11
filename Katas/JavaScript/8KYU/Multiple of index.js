615bf36cf94f6f6bb54979dc0180a335693346e7
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-11 12:50:11 -0500

    Added 8KYU

--- Katas/JavaScript/8KYU/Multiple of index.js
+++ Katas/JavaScript/8KYU/Multiple of index.js
@@ -8,3 +8,12 @@ function multipleOfIndex(array) {
     }
     return answer;
 }
+
+//Other Solutions:
+
+//function multipleOfIndex(array) {
+//    return array.filter((num, i) => num % i === 0);
+//}
+
+//multipleOfIndex = (array) => result = array.filter((number, i) => number % i === 0);
+
