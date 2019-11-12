243e997137eb10c93a5ef6b4289726df94fe8e47
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-12 14:21:52 -0500

    Added 8KYU

---
+++ Katas/C#/8KYU/MakeUpperCase.cs
@@ -0,0 +1,14 @@
+public class Kata
+{
+    public static string MakeUpperCase(string str)
+    {
+        return str.ToUpper();
+    }
+}
+
+//Other Solutions
+
+//public class Kata
+//{
+//    public static string MakeUpperCase(string str) => str.ToUpper();
+//}
