d577276f8d8e2c448f926a0814ecc64e9a82ec5a
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-21 09:17:26 -0500

    Added 7KYU

---
+++ Katas/JavaScript/7KYU/Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,10 @@
+function getSum(a, b) {
+    let answer = 0;
+    if (a <= b)
+        for (let i = a; i <= b; i++)
+            answer += i;
+    else
+        for (let j = b; j <= a; j++)
+            answer += j;
+    return answer;
+}
