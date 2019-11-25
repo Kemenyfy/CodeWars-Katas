178a211a195f942280b99450eabb43cfd9ff1221
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-11-25 14:21:09 -0500

    Added 6KYU

--- Katas/C#/6KYU/Multiples of 3 or 5.cs
+++ Katas/C#/6KYU/Multiples of 3 or 5.cs
@@ -3,7 +3,7 @@ public static class Kata
   public static int Solution(int value)
   {
     int result = 0;
-    for (int x=1; x<value;x++) { if (x % 3 == 0 || x % 5 == 0) {result += x;}}
+    for (int x = 1; x < value; x++) { if (x % 3 == 0 || x % 5 == 0) {result += x;}}
     return result;
   }
 }
