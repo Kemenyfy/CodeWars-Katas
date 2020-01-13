318d6a2a165fdb8341355a419823c6ee6e091812
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2020-01-13 11:45:37 -0500

    Added 7 Kyu

--- Katas/JavaScript/7KYU/Maximum Product.js
+++ Katas/JavaScript/7KYU/Maximum Product.js
@@ -14,3 +14,27 @@ function adjacentElementsProduct(array) {
     return maxProduct;
 
 }
+
+// A different Solution:
+
+function adjacentElementsProduct(array) {
+
+    let answer = [];
+    for (i = 1; i < array.length; i++) {
+        answer.push(array[i - 1] * array[i])
+    }
+    return Math.max.apply(null, answer);
+
+}
+
+// Or:
+
+function adjacentElementsProduct(array) {
+
+    let newArray = [];
+    for (i = 0; i < array.length - 1; i++) {
+        newArray.push(array[i] * array[i + 1])
+    }
+    return Math.max(...newArray)
+
+}
